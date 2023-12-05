3d4c6018e42b389489464e492a39af4734446bce
Joseph Bartoszczyk <jbartoszczyk@uiowa.edu>
2023-12-05 17:15:49 -0600

    init

---
+++ modern-db/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function App() {
+  return (
+      <Router>
+        <div>
+            <Routes>
+
+            </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ modern-db/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Neo4jProvider,createDriver} from "use-neo4j";
+const driver = createDriver('neo4j','localhost',7687,'admin','password')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Neo4jProvider driver={driver}>
+          <App />
+      </Neo4jProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
